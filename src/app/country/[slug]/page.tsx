be26af0872fee033b850b054ce41c5b065cd6e3c
hiepxuan <hiepxuan2006@gmail.com>
2024-12-14 22:45:38 +0700

    update

--- src/app/country/[slug]/page.tsx
+++ src/app/country/[slug]/page.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 
-type Props = any
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const page = (props: Props) => {
+const page = () => {
   return <div>page</div>
 }
