4181df2e1c97b816bf9a90fca85cb731a5196802
hiepxuan <hiepxuan2006@gmail.com>
2024-12-14 22:54:59 +0700

    update

--- src/app/country/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react'
-
-type Props = any
-
-const page = (props: Props) => {
-  return <div>page</div>
-}
